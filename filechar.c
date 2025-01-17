b6d8957458fa666de865d57522ac373c8a4d1387
RobertJN64 <robertjnies@gmail.com>
2025-01-17 15:09:33 -0500

    countChar and printCounts done

--- filechar.c
+++ filechar.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
-bool countChar(char * filename, int * counts, int size)
-{
+bool countChar(char *filename, int *counts, int size) {
   // open a file whose name is filename for reading
-  // if fopen fails, return false. Do NOT fclose
+  FILE *file = fopen(filename, "r");
+
+  if (file == NULL) { // if fopen fails, return false. Do NOT fclose
+    return false;
+  }
+
   // if fopen succeeds, read every character from the file
-  //
-  // if a character (call it onechar) is between
-  // 0 and size - 1 (inclusive), increase
-  // counts[onechar] by one
-  // You should *NOT* assume that size is 256
-  // reemember to call fclose
+  char onechar;
+  do {
+    onechar = getc(file);
+    // if a character (call it onechar) is between
+    // 0 and size - 1 (inclusive), increase
+    // counts[onechar] by one
+    // You should *NOT* assume that size is 256
+    if (0 <= onechar && onechar < size) {
+      counts[(int)onechar] += 1;
+    }
+  } while (onechar != EOF);
+
   // you may assume that counts already initialized to zero
   // size is the size of counts
   // you may assume that counts has enough memory space
@@ -20,18 +29,23 @@ bool countChar(char * filename, int * counts, int size)
   // hint: use fgetc
   // Please read the document of fgetc carefully, in particular
   // when reaching the end of the file
-  //
+
+  fclose(file); // remember to call fclose
   return true;
 }
 
+void printCounts(int *counts, int size) { // ind is between 0 and size - 1 (inclusive)
+  for (int count_idx = 0; count_idx < size; count_idx++) {
+    if (counts[count_idx] > 0) { // if counts[ind] is zero, do not print
 
-void printCounts(int * counts, int size)
-{
-  // print the values in counts in the following format
-  // each line has three items:
-  // ind, onechar, counts[ind]
-  // ind is between 0 and size - 1 (inclusive)
-  // onechar is printed if ind is between 'a' and 'z' or
-  // 'A' and 'Z'. Otherwise, print space
-  // if counts[ind] is zero, do not print
+      // onechar is printed if ind is between 'a' and 'z' or
+      // 'A' and 'Z'. Otherwise, print space
+      char onechar = ('a' <= count_idx && count_idx <= 'z') || ('A' <= count_idx && count_idx <= 'Z') ? count_idx : ' ';
+
+      // print the values in counts in the following format
+      // each line has three items:
+      // ind, onechar, counts[ind]
+      printf("%d, %c, %d\n", count_idx, onechar, counts[count_idx]);
+    }
+  }
 }
